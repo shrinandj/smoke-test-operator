e4e4346e45bffa73b7b122063795dc54cb982bbf
Shri Javadekar <shrinand_javadekar@intuit.com>
2018-05-16 12:45:19 -0700

    Run test mentioned in the annotation.

--- pkg/stub/handler.go
+++ pkg/stub/handler.go
@@ -41,18 +41,41 @@ func fileCopy(sourceFile string, destFile string) {
 	}
 }
 
+func updateCR(cr *v1alpha1.SmokeTest, testOutput string) {
+	cr.Status.TestOutput = testOutput
+	err := action.Update(cr)
+	if err != nil {
+		logrus.Errorf("Failed to update cr: %v", err)
+	}
+	logrus.Infof("Successfully updated TestOutput for smoketest %s", cr.Name)
+}
+
 func (h *Handler) Handle(ctx types.Context, event types.Event) error {
 	switch cr := event.Object.(type) {
 	case *v1alpha1.SmokeTest:
 		if cr.Status.TestOutput != "" {
-			// SmokeTest has already been executed previously.
+			// SmokeTest has been processed previously.
+			return nil
+		}
+
+		testToRun := "test.sh"
+		if cr.Annotations != nil {
+			if val, ok := cr.Annotations["test_to_run"]; ok {
+				testToRun = val
+				logrus.Infof("Found test to run annotation: %s", testToRun)
+			}
+		}
+		testFile := "/smoke-tests/" + testToRun
+		if _, err := os.Stat(testFile); os.IsNotExist(err) {
+			logrus.Infof("Test %s does not exist for %s.", testFile, cr.Name)
+			updateCR(cr, "Test "+testFile+" does not exist")
 			return nil
 		}
 
 		// Execute script here
 		// 1. Copy the file to /tmp.
-		destFile := "/tmp/test.sh"
-		fileCopy("/smoke-tests/test.sh", destFile)
+		destFile := "/tmp/" + testToRun
+		fileCopy(testFile, destFile)
 		defer os.Remove(destFile)
 		logrus.Infof("Successfully copied script to %s", destFile)
 
@@ -63,12 +86,7 @@ func (h *Handler) Handle(ctx types.Context, event types.Event) error {
 			return err
 		}
 
-		cr.Status.TestOutput = string(op)
-		err = action.Update(cr)
-		if err != nil {
-			logrus.Errorf("Failed to update cr: %v", err)
-		}
-		logrus.Infof("Successfully executed script for smoketest %s", cr.Name)
+		updateCR(cr, string(op))
 	}
 	return nil
 }
